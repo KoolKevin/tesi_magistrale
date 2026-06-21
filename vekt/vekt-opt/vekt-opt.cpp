ae9756deff24014ffc6be44940ece1c9b8a4e500
KoolKevin <kevinkoltraka1011@gmail.com>
2026-06-21 14:03:59 +0200

    lowering pipeline funzionante

--- vekt/vekt-opt/vekt-opt.cpp
+++ vekt/vekt-opt/vekt-opt.cpp
@@ -1,13 +1,4 @@
-//===- standalone-opt.cpp ---------------------------------------*- C++ -*-===//
-//
-// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-
-#include "mlir/Dialect/Arith/IR/Arith.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Affine/Transforms/Passes.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/InitAllDialects.h"
 #include "mlir/InitAllPasses.h"
@@ -15,28 +6,58 @@
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 #include "mlir/Transforms/Passes.h"
 
+#include "mlir/Conversion/AffineToStandard/AffineToStandard.h"
+#include "mlir/Conversion/ControlFlowToLLVM/ControlFlowToLLVM.h"
+#include "mlir/Conversion/FuncToLLVM/ConvertFuncToLLVM.h"
+#include "mlir/Conversion/MemRefToLLVM/MemRefToLLVM.h"
+#include "mlir/Conversion/Passes.h"
+#include "mlir/Conversion/SCFToControlFlow/SCFToControlFlow.h"
+#include "mlir/Conversion/UBToLLVM/UBToLLVM.h"
+#include "mlir/Conversion/VectorToLLVM/ConvertVectorToLLVM.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
+
 #include "Standalone/StandaloneDialect.h"
 #include "Standalone/StandalonePasses.h"
 
 int main(int argc, char **argv) {
   mlir::registerAllPasses();
   mlir::standalone::registerPasses();
-  // TODO: Register standalone passes here.
 
   mlir::PassPipelineRegistration<>(
-      "my-pipeline", "Pipeline CIR: canonicalize + hoist-allocas",
-      [](mlir::OpPassManager &pm) {
+      "vekt16", "naive vectorization", [](mlir::OpPassManager &pm) {
         pm.addPass(mlir::createCanonicalizerPass());
+
+        mlir::affine::AffineVectorizeOptions opts;
+        opts.vectorSizes = {16};
+        pm.addPass(mlir::affine::createAffineVectorize(opts));
+
+        // lower to llvm
+        pm.addPass(mlir::createLowerAffinePass());
+        pm.addPass(mlir::createSCFToControlFlowPass());
+        pm.addPass(mlir::createConvertControlFlowToLLVMPass());
+        pm.addPass(mlir::createConvertVectorToLLVMPass());
+        pm.addPass(mlir::createArithToLLVMConversionPass());
+        pm.addPass(mlir::createConvertFuncToLLVMPass());
+        pm.addPass(mlir::createUBToLLVMConversionPass());
+
+        pm.addPass(mlir::createReconcileUnrealizedCastsPass());
+        pm.addPass(mlir::createFinalizeMemRefToLLVMConversionPass());
+
+        // cleanup
+        pm.addPass(mlir::createCanonicalizerPass());
+        pm.addPass(mlir::createSCCPPass());
+        pm.addPass(mlir::createCSEPass());
+        pm.addPass(mlir::createSymbolDCEPass());
       });
 
   mlir::DialectRegistry registry;
-
-  registry.insert<mlir::standalone::StandaloneDialect,
-                  mlir::arith::ArithDialect, mlir::func::FuncDialect>();
+  //   registry.insert<mlir::standalone::StandaloneDialect,
+  //                   mlir::arith::ArithDialect, mlir::func::FuncDialect>();
   // Add the following to include *all* MLIR Core dialects, or selectively
   // include what you need like above. You only need to register dialects that
   // will be *parsed* by the tool, not the one generated
-  // registerAllDialects(registry);
+  registerAllDialects(registry);
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "Standalone optimizer driver\n", registry));
